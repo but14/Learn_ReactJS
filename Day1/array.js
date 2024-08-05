5ea143044e7323b466d560b3943b024ec801d2b9
Longngoo <100961248+but14@users.noreply.github.com>
2024-08-05 22:42:57 +0700

    code

---
+++ Day1/array.js
@@ -0,0 +1,54 @@
+const numbers = [0, 3.14, 9.81, 98,6, 100]; //array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon']; //array of string, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion' ,'Carrot']; //array of string, vegetables
+const animalProduct = ['Milk', 'meat', 'butter', 'yoghurt']; //array of string
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB']; //Array of strign, tech
+const countries = ['Finland', 'VietNam', 'Norway', 'Iceland']; //array of countries
+
+
+//print the array and its length
+
+console.log('Numbers:', numbers);
+console.log('Number of numbers:', numbers.length);
+
+
+console.log('Fruits:', fruits);
+console.log('Number of Fruits:', fruits.length);
+
+console.log('Vegetables: ', vegetables);
+console.log('Number of Vegetables:', vegetables.length);
+
+console.log('Animal Product:', animalProduct);
+console.log('Number of Animal Product:', animalProduct.length);
+
+console.log('Tech:', webTechs);
+console.log('Number of Tech:', webTechs.length);
+
+console.log('Animal Country:', countries);
+console.log('Number of contries:', countries.length);
+
+const arr = [
+    'Asabenh',
+    250,
+    true,
+    {country: 'Finland', city: 'Helsinki'},
+    {skill: ['HTML', 'CSS', 'JS']},
+]
+
+console.log(arr);
+
+
+let js = 'JavaScript';
+const charsInJavaScript = js.split('');
+
+console.log(charsInJavaScript)
+
+let dcm = 'Document';
+const charsInJavaScript2 = dcm.split('');
+console.log(charsInJavaScript2);
+
+const pt = 'Python';
+const charsInJavaScript3 = pt.split('')
+console.log(charsInJavaScript3);
+
+
