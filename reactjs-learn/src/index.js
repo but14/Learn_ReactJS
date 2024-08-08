e359e8af5aa2673513e9e06b099d3c55647b16e9
Longngoo <100961248+but14@users.noreply.github.com>
2024-08-08 14:25:03 +0700

    code 0808

--- reactjs-learn/src/index.js
+++ reactjs-learn/src/index.js
@@ -1,65 +1,96 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDom from "react-dom";
 
-const jsxElement = <h1>This is a JSX element</h1>
+// JSX element, header
 
+const welcome = "Welcome to 30 Days Of react";
+const title = "Getting started React";
+const subtitle = "JavaScript Libraby";
+const author = {
+  firstName: "Asabeneh",
+  lastName: "Yetayeh",
+};
+const date = "Aug 08, 2024";
 
-
-const headerStyles = {
-  backgroundColor: 'lightblue',
-  fontFamily: 'Arial',
-  padding: 25,
-  lineHeight: 1.5,
-}
+// Jsx element, header
 
 const header = (
-  <header style={headerStyles}>
-    <h1>Welcome to 30 days of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>Park Hoang Long</p>
-    <small>
-      Aug 08, 2024
-    </small>
+  <header>
+    <div className="header-wrapper">
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName};
+      </p>
+      <small>Date: {date}</small>
+    </div>
   </header>
-)
+);
 
-const mainStyles = {
-  backgroundColor: '#F3F0F5',
-}
+const numOne = 3;
+
+const numTwo = 2;
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+);
+
+const yearBorn = 1820;
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearBorn;
+
+const personAge = (
+  <p>
+    {" "}
+    {author.firstName} {author.lastName} is {age} years old.
+  </p>
+);
+
+// JSX main
+
+const techs = ["HTML", "CSS", "Javascript"];
+const techsFormatted = techs.map((techs) => <li>{techs}</li>);
 
 // JSX element, main
 const main = (
-  <main style={mainStyles}>
-    <p>Prerequisite to get started react.js:</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul>
+  <main>
+    <div className="main-wrapper">
+      <p>
+        Prerequisite to get started{""}
+        <strong>
+          <em>React. js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+    </div>
   </main>
-)
+);
 
+const copyRight = "Copyright 2020";
 
-const footerStyles = {
-  backgroundColor: '#61DBFB',
-}
+// JSX element, footer
 
 const footer = (
-  <footer   style={footerStyles}>
-    <p>Copyright By Ngow</p>
+  <footer>
+    <div className="footer-wrapper">
+      <p>{copyRight}</p>
+    </div>
   </footer>
-)
+);
 
-// JSX element, app, a container or a parent
 const app = (
-  <div>
+  <div className="app">
     {header}
     {main}
     {footer}
   </div>
-)
+);
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(app, rootElement);
+const rootElement = document.getElementById("root");
+ReactDom.render(app, rootElement); // render the JSX element to the DOM
